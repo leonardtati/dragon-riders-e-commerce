a951b450d1fc74d8bc6f420942fe9b7e22edd614
Leonard Poissant <leonardtati@gmail.com>
2020-04-12 10:13:38 -0400

    dropDown

--- client/src/components/DropDown/DropDown.js
+++ client/src/components/DropDown/DropDown.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-const Countries = () => {
-  return <div />;
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import {
+  requestCountries,
+  receiveCountries,
+  receiveCountriesError,
+} from "../../actions";
+
+const DropDown = () => {
+  const dispatch = useDispatch();
+  const countriesStatus = useSelector((state) => state.country.status);
+  const getCountries = useSelector((state) => state.country.countries);
+  const [countryValue, setCountryValue] = React.useState("");
+
+  const countryArray = getCountries.countries;
+
+  console.log("Countries", countryArray);
+
+  React.useEffect(() => {
+    dispatch(requestCountries());
+    fetch("/countries")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(receiveCountries(data));
+      })
+      .catch((error) => {
+        dispatch(receiveCountriesError());
+      });
+  }, []);
+
+  //MOVE THE LOADING DIV IN THE HOME COMPONENT WITH THE APPROPRIATE SELECTOR
+
+  if (countriesStatus === "loading") {
+    return <div>LOADING</div>;
+  }
+
+  return (
+    <Wrapper>
+      <select onChange={(ev) => setCountryValue(ev.target.value)}>
+        {countryArray.map((country) => {
+          return <option value={country}>{country}</option>;
+        })}
+      </select>
+    </Wrapper>
+  );
 };
 
-export default Countries;
+const Wrapper = styled.div``;
+
+export default DropDown;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import country from "./country-reducer";
+
+export default combineReducers({ country });
